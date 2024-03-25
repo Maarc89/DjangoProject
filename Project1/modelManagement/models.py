b0a9c73d80a147f56dae15769f1ef525efb49de0
Marc <124775478+Maarc89@users.noreply.github.com>
2024-03-25 16:21:43 +0100

    canvis models.py

--- Project1/modelManagement/models.py
+++ Project1/modelManagement/models.py
@@ -1,9 +1,21 @@
 from django.db import models
 
-class Person(models.Model):
+class Politic(models.Model):
   firstname = models.CharField(max_length=255)
   lastname = models.CharField(max_length=255)
+  carrec = models.CharField(max_length=255)
   phone = models.IntegerField(null=True)
 
   def __str__(self):
     return f"{self.firstname} {self.lastname}"
+
+class PartitPolitic(models.Model):
+  name = models.CharField(max_length=255)
+  leader = models.CharField(max_length=255)
+  ideology = models.CharField(max_length=255)
+  description = models.TextField()
+
+class Reunion(models.Model):
+  date = models.DateField()
+  hours = models.IntegerField()
+  ubication = models.CharField(max_length=255)
