ad981dc93800b44336ad367ac69c02a2025d1e3b
AdriaCreus <155093465+AdriaCreus@users.noreply.github.com>
2024-03-25 20:37:09 +0100

    Primera vista v2

--- Project1/Project1/settings.py
+++ Project1/Project1/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-_5ixzo($8_j7-h(a$2uk!4#39ijnz13$p3oj(l-mk+rh64$9=9
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -70,7 +68,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Project1.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
@@ -80,7 +77,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -100,7 +96,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
@@ -112,18 +107,21 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
 
-#Add this in your settings.py file:
+# Add this in your settings.py file:
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    BASE_DIR / 'static',
+    BASE_DIR / 'media'
 ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
--- Project1/modelManagement/models.py
+++ Project1/modelManagement/models.py
@@ -6,6 +6,7 @@ class Politic(models.Model):
   carrec = models.CharField(max_length=255, null=True)
   partit = models.CharField(max_length=255, null=True)
   description = models.TextField(null=True)
+#foto = models.ImageField(upload_to='images/',null=True)
 
   def __str__(self):
     return f"{self.firstname} {self.lastname}"
