42901e832f870a1e62e25013b8b5bc22ee83920d
AdriaCreus <155093465+AdriaCreus@users.noreply.github.com>
2024-03-25 19:15:16 +0100

    Primera Vista

--- Project1/modelManagement/models.py
+++ Project1/modelManagement/models.py
@@ -3,8 +3,9 @@ from django.db import models
 class Politic(models.Model):
   firstname = models.CharField(max_length=255)
   lastname = models.CharField(max_length=255)
-  carrec = models.CharField(max_length=255)
-  phone = models.IntegerField(null=True)
+  carrec = models.CharField(max_length=255, null=True)
+  partit = models.CharField(max_length=255, null=True)
+  description = models.TextField(null=True)
 
   def __str__(self):
     return f"{self.firstname} {self.lastname}"
